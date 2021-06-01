d3d8f616c435edf63fded941dc113289a1ed5e2f
Anton Kovunov <anton.kovunov@gmail.com>
2021-06-01 13:13:50 -0400

    BACKLOG-16036 fixed handler to work for two actions

--- src/main/java/org/jahia/modules/sam/events/TaskRegisterTaskEventHandler.java
+++ src/main/java/org/jahia/modules/sam/events/TaskRegisterTaskEventHandler.java
@@ -7,20 +7,18 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
 
-@Component(service = EventHandler.class, immediate = true, property = EventConstants.EVENT_TOPIC + "=org/jahia/module/sam/register")
+@Component(service = EventHandler.class, immediate = true,
+        property = {EventConstants.EVENT_TOPIC + "=org/jahia/modules/sam/TaskRegistryService/REGISTER",
+                EventConstants.EVENT_TOPIC + "=org/jahia/modules/sam/TaskRegistryService/UNREGISTER"})
 public class TaskRegisterTaskEventHandler implements EventHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(TaskRegisterTaskEventHandler.class);
 
     private static final String SERVICE_PROPERTY = "service";
     public static final String NAME_PROPERTY = "name";
     private static final String STARTED_PROPERTY = "started";
-    private static final String ACTION_PROPERTY = "action";
 
     private TaskRegistryService taskRegistryService;
 
@@ -33,12 +31,10 @@ public class TaskRegisterTaskEventHandler implements EventHandler {
     public void handleEvent(Event event) {
         TaskDetails taskInfo = new TaskDetails(event.getProperty(SERVICE_PROPERTY).toString(), event.getProperty(NAME_PROPERTY).toString());
         taskInfo.setStarted(event.getProperty(STARTED_PROPERTY) == null ? null : (Calendar) event.getProperty(STARTED_PROPERTY));
-        if ("REGISTER".equals(event.getProperty(ACTION_PROPERTY))) {
-            taskRegistryService.registerTask(taskInfo);
-        } else {
+        if (event.getTopic().endsWith("UNREGISTER")) {
             taskRegistryService.unregisterTask(taskInfo.getName());
+        } else {
+            taskRegistryService.registerTask(taskInfo);
         }
-        taskRegistryService.registerTask(taskInfo);
-        logger.info("Task is registered with name: {} and service: {}", event.getProperty(NAME_PROPERTY), event.getProperty(SERVICE_PROPERTY));
     }
 }
