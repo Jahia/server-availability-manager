a75818bdda5c60eb590a0c79276d4ad161181695
Rafael Fonseca <rfonseca85@yahoo.ca>
2021-06-03 18:05:29 -0400

    Adding shutdown.spec.final back. Removed it by mistake :)

---
+++ tests/cypress/integration/api/shutdown.spec.final.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import { apollo } from '../../support/apollo'
+import { DocumentNode } from 'graphql'
+
+describe('Shutdown via API - mutation.admin.serverAvailabilityManager.shutdown', () => {
+    let GQL_SHUTDOWN: DocumentNode
+
+    before('load graphql file and create test dataset', () => {
+        GQL_SHUTDOWN = require(`graphql-tag/loader!../../fixtures/shutdown.graphql`)
+    })
+
+    it('Shutdown success', function () {
+        //This test must be the last test for obviously reason
+        cy.task('apolloNode', {
+            baseUrl: Cypress.config().baseUrl,
+            authMethod: { username: 'root', password: Cypress.env('SUPER_USER_PASSWORD') },
+            mode: 'mutate',
+            variables: {
+                timeout: 100,
+            },
+            query: GQL_SHUTDOWN,
+        }).then((response: any) => {
+            cy.log(JSON.stringify(response))
+            expect(response.data.admin.serverAvailabilityManager.shutdown).to.be.true
+        })
+    })
+})
