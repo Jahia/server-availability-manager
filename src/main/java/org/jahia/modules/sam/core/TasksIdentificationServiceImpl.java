c18a99458e4baf52766930ba1640923d9f88487d
Anton Kovunov <anton.kovunov@gmail.com>
2021-06-09 11:12:44 -0400

    BACKLOG-16124 implemented thread dump string matching (#25)
    * BACKLOG-16124 implemented thread dump string matching
    
    * BACKLOG-16124 implemented stack iteration from deepest
    
    * Refactor using streams
    
    Co-authored-by: Thomas Draier <tdraier@jahia.com>

--- src/main/java/org/jahia/modules/sam/core/TasksIdentificationServiceImpl.java
+++ src/main/java/org/jahia/modules/sam/core/TasksIdentificationServiceImpl.java
@@ -1,34 +1,44 @@
 package org.jahia.modules.sam.core;
 
-import org.jahia.data.templates.ModuleState;
 import org.jahia.modules.sam.TaskRegistryService;
 import org.jahia.modules.sam.TasksIdentificationService;
 import org.jahia.modules.sam.model.TaskDetails;
-import org.jahia.services.scheduler.SchedulerService;
-import org.jahia.services.templates.JahiaTemplateManagerService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.quartz.SchedulerException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Map;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 @Component(immediate = true, service = TasksIdentificationService.class)
 public class TasksIdentificationServiceImpl implements TasksIdentificationService {
-    private static final Logger logger = LoggerFactory.getLogger(TasksIdentificationServiceImpl.class);
-    private static final Pattern THREAD_DUMP_PATTERN = Pattern.compile("importSiteZip|importContent");
+    private enum ThreadDumpTask {
+        IMPORT_ZIP("org.jahia.services.importexport.ImportExportBaseService.importZip"),
+        IMPORT_XML("org.jahia.services.importexport.ImportExportBaseService.importXML"),
+        BACKGROUND_JOB("org.jahia.services.scheduler.BackgroundJob.execute"),
+        BUNDLE_START("org.jahia.services.modulemanager.impl.ModuleManagerImpl.start"),
+        BUNDLE_INSTALL("org.jahia.services.modulemanager.impl.ModuleManagerImpl.install");
 
-    private SchedulerService schedulerService;
+        private final String fullSignature;
 
-    private JahiaTemplateManagerService templateManagerService;
+        public String getFullSignature() {
+            return fullSignature;
+        }
+
+        ThreadDumpTask(String fullSignature) {
+            this.fullSignature = fullSignature;
+        }
+    }
+
+
+    private final Map<String, ThreadDumpTask> taskSignatures = EnumSet.allOf(ThreadDumpTask.class).stream()
+            .collect(toMap(ThreadDumpTask::getFullSignature, threadDumpTask -> threadDumpTask));
 
     private TaskRegistryService taskRegistryService;
 
@@ -37,50 +47,23 @@ public class TasksIdentificationServiceImpl implements TasksIdentificationServic
         this.taskRegistryService = taskRegistryService;
     }
 
-    @Reference
-    public void setSchedulerService(SchedulerService schedulerService) {
-        this.schedulerService = schedulerService;
-    }
-
-    @Reference
-    public void setTemplateManagerService(JahiaTemplateManagerService templateManagerService) {
-        this.templateManagerService = templateManagerService;
-    }
-
     @Override
     public Stream<TaskDetails> getRunningTasksStream() {
-        try {
-            Stream<TaskDetails> backgroundJobsData = schedulerService.getAllActiveJobs()
-                    .stream()
-                    .map(jobDetail -> new TaskDetails("Background job", jobDetail.getName()));
-
-            Stream<TaskDetails> modulesData = templateManagerService.getModuleStates()
-                    .entrySet()
-                    .stream()
-                    .filter(entry -> EnumSet.of(ModuleState.State.SPRING_STARTING, ModuleState.State.STARTING, ModuleState.State.STOPPING, ModuleState.State.WAITING_TO_BE_IMPORTED)
-                            .contains(entry.getValue().getState()))
-                    .map(entry -> new TaskDetails("Module deployment", entry.getKey().getLocation() + "-" + entry.getValue().getState()));
-
-            return Stream.of(backgroundJobsData, modulesData, getTasksFromThreadDump(), taskRegistryService.getRegisteredTasks())
-                    .reduce(Stream::concat)
-                    .orElseGet(Stream::empty);
-        } catch (SchedulerException e) {
-            logger.error("Can't get data from the scheduler service: {}", e.getMessage());
-        }
-        return null;
+        return Stream.of(getDetectedTasks(), taskRegistryService.getRegisteredTasks())
+                .reduce(Stream::concat)
+                .orElseGet(Stream::empty);
     }
 
-    private Stream<TaskDetails> getTasksFromThreadDump() {
-        List<TaskDetails> tasksToCheck = new ArrayList<>();
-        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-        for(ThreadInfo threadInfo : threadMXBean.dumpAllThreads(true, true)) {
-           StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
-            for (StackTraceElement stackTraceElement : stackTraceElements) {
-                if (THREAD_DUMP_PATTERN.matcher(stackTraceElement.getMethodName()).find()) {
-                    tasksToCheck.add(new TaskDetails("Thread dump", stackTraceElement.getMethodName()));
-                }
-            }
-        }
-        return tasksToCheck.stream();
+    private Stream<TaskDetails> getDetectedTasks() {
+        return Arrays.stream(ManagementFactory.getThreadMXBean().dumpAllThreads(false, false))
+                .map(ThreadInfo::getStackTrace)
+                .flatMap(this::processStackTrace);
+    }
+
+    private Stream<TaskDetails> processStackTrace(StackTraceElement[] stackTraceElements) {
+        return IntStream.rangeClosed(1, stackTraceElements.length).mapToObj(i -> stackTraceElements[stackTraceElements.length - i])
+                .map(element -> element.getClassName() + "." + element.getMethodName())
+                .filter(taskSignatures::containsKey).limit(1)
+                .map(key -> new TaskDetails("core", taskSignatures.get(key).toString()));
     }
 }
