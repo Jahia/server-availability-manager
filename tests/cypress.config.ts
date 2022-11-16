d7d7d9d71809010718123eda361b681b9487bcd1
Alex Karmanov <alexandrk_@hotmail.com>
2022-11-16 14:39:26 -0500

    BACKLOG-20131 Upgrade cypress image

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     viewportHeight: 768,
     e2e: {
         // We've imported your old cypress plugins here.
-        // You may want to clean this up later by importing these.
+        // You may want to clean this up later by importing tcleahese.
         setupNodeEvents(on, config) {
             return require('./cypress/plugins/index.js')(on, config);
         },
