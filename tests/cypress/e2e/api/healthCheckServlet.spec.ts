1783f8834bd4a59c8460c30a6297871d6dfdf6f6
Geofrey Flores <gflores@jahia.com>
2024-06-03 11:28:48 -0400

    Fix lint

--- tests/cypress/e2e/api/healthCheckServlet.spec.ts
+++ tests/cypress/e2e/api/healthCheckServlet.spec.ts
@@ -1,4 +1,4 @@
-const healthcheck = (qs) => {
+const healthcheck = qs => {
     return cy.request({
         url: `${Cypress.config().baseUrl}/modules/healthcheck`,
         qs,
@@ -16,27 +16,27 @@ const healthcheck = (qs) => {
 
 describe('healthcheck REST API test', () => {
     it('should return using "includes" parameter', () => {
-        console.log("Return with no filter");
+        console.log('Return with no filter');
         healthcheck({}).should(response => {
             expect(response.status).to.eq(200);
             expect(response.body.probes).to.not.be.empty;
         });
 
-        console.log("Return with empty filter");
+        console.log('Return with empty filter');
         healthcheck({includes: undefined}).should(response => {
             expect(response.status).to.eq(200);
             expect(response.body.probes).to.not.be.empty;
         });
 
-        console.log("Return one probe");
-        healthcheck({includes: "FileDatastore"}).should(response => {
+        console.log('Return one probe');
+        healthcheck({includes: 'FileDatastore'}).should(response => {
             expect(response.status).to.eq(200);
             expect(response.body.probes?.length).to.eq(1);
             expect(response.body.probes[0]?.name).to.eq('FileDatastore');
         });
 
-        console.log("Return more than one probe");
-        healthcheck({includes: "FileDatastore,DBConnectivity"}).should(response => {
+        console.log('Return more than one probe');
+        healthcheck({includes: 'FileDatastore,DBConnectivity'}).should(response => {
             expect(response.status).to.eq(200);
             expect(response.body.probes.length).to.be.eq(2);
             const probeNames = response.body.probes?.map(r => r.name);
@@ -44,19 +44,18 @@ describe('healthcheck REST API test', () => {
             expect('DBConnectivity').to.be.oneOf(probeNames);
         });
 
-        console.log("Filter with only invalid probe");
-        healthcheck({includes: "UndefinedProbe"}).should(response => {
+        console.log('Filter with only invalid probe');
+        healthcheck({includes: 'UndefinedProbe'}).should(response => {
             expect(response.status).to.eq(200);
             expect(response.body.probes).to.be.empty;
         });
 
-        console.log("Filter invalid probe");
-        healthcheck({includes: "FileDatastore,UndefinedProbe"}).should(response => {
+        console.log('Filter invalid probe');
+        healthcheck({includes: 'FileDatastore,UndefinedProbe'}).should(response => {
             expect(response.status).to.eq(200);
             expect(response.body.probes.length).to.be.eq(1);
             const probeNames = response.body.probes?.map(r => r.name);
             expect('FileDatastore').to.be.oneOf(probeNames);
         });
     });
-
 });
