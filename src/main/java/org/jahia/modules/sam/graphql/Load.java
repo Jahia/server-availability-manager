d4b50916c7f7277a1eaee56e72feec411451d6cf
Jérôme Blanchard <jblanchard@jahia.com>
2024-06-03 22:20:23 +0200

    TECH-1808 Adapt G to latest core modifications about Load Average (before full migration to SAM module)

--- src/main/java/org/jahia/modules/sam/graphql/Load.java
+++ src/main/java/org/jahia/modules/sam/graphql/Load.java
@@ -2,28 +2,32 @@ package org.jahia.modules.sam.graphql;
 
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
-import org.jahia.utils.JCRNodeCacheLoadAverage;
-import org.jahia.utils.JCRSessionLoadAverage;
-import org.jahia.utils.RequestLoadAverage;
+import org.jahia.utils.load.*;
 
 @GraphQLDescription("Server load")
 public class Load {
 
     @GraphQLField
+    @GraphQLDescription("Get Thread load")
+    public LoadProvider getThread() {
+        return new LoadProvider(LoadAverageMonitor.getInstance().findProviderForName(ThreadLoadAverage.NAME).orElse(null));
+    }
+
+    @GraphQLField
     @GraphQLDescription("Get requests load")
-    public LoadValue getRequests() {
-        return new LoadValue(RequestLoadAverage.getInstance());
+    public LoadProvider getRequests() {
+        return new LoadProvider(LoadAverageMonitor.getInstance().findProviderForName(RequestLoadAverage.NAME).orElse(null));
     }
 
     @GraphQLField
     @GraphQLDescription("Get JCR Sessions load")
-    public LoadValue getSessions() {
-        return new LoadValue(JCRSessionLoadAverage.getInstance());
+    public LoadProvider getSessions() {
+        return new LoadProvider(LoadAverageMonitor.getInstance().findProviderForName(JCRSessionLoadAverage.NAME).orElse(null));
     }
 
     @GraphQLField
     @GraphQLDescription("Get JCR Node Cache load across active sessions")
-    public LoadValue getCachedNodes() {
-        return new LoadValue(JCRNodeCacheLoadAverage.getInstance());
+    public LoadProvider getCachedNodes() {
+        return new LoadProvider(LoadAverageMonitor.getInstance().findProviderForName(JCRNodeCacheLoadAverage.NAME).orElse(null));
     }
 }
