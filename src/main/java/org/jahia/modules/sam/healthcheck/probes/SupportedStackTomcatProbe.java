d4a687e9c55222625f0f087c40bba45bfc330141
akarmanov <alexandrk_@hotmail.com>
2024-06-12 10:04:08 -0400

    BACKLOG-22896 Add SupportedStackTomcatProbe

---
+++ src/main/java/org/jahia/modules/sam/healthcheck/probes/SupportedStackTomcatProbe.java
@@ -0,0 +1,59 @@
+package org.jahia.modules.sam.healthcheck.probes;
+
+import org.jahia.commons.Version;
+import org.jahia.modules.sam.Probe;
+import org.jahia.modules.sam.ProbeSeverity;
+import org.jahia.modules.sam.ProbeStatus;
+import org.jahia.settings.SettingsBean;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Map;
+
+@Component(immediate = true, service = Probe.class)
+public class SupportedStackTomcatProbe implements Probe {
+
+    Version supportedTomcatVersion = new Version("9.0.85");
+    @Override
+    public String getName() {
+        return "SupportedStackTomcat";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This probe give information about Tomcat version Jahia is running on.";
+    }
+
+    @Override
+    public ProbeStatus getStatus() {
+        String usedVersion = SettingsBean.getInstance().getServletContext().getServerInfo();
+        Version currentVersion = new Version(usedVersion.split("/")[1]);
+
+        if (supportedTomcatVersion.compareTo(currentVersion) == 0) {
+            return new ProbeStatus("Jahia is deployed on the latest supported Tomcat 9 version", ProbeStatus.Health.GREEN);
+        }
+
+        if (supportedTomcatVersion.compareTo(currentVersion) < 0) {
+            return new ProbeStatus("Jahia is deployed on a Tomcat 9 version that has not been validated by Jahia", ProbeStatus.Health.YELLOW);
+        }
+
+        if (supportedTomcatVersion.compareTo(currentVersion) > 0 && currentVersion.getMajorVersion() > supportedTomcatVersion.getMajorVersion()) {
+            return new ProbeStatus("Jahia is deployed on a supported Tomcat 9 version but not the latest one supported by Jahia, we encourage you to upgrade to use version", ProbeStatus.Health.YELLOW);
+        }
+
+        if (currentVersion.getMajorVersion() < supportedTomcatVersion.getMajorVersion()) {
+            return new ProbeStatus("Jahia is deployed on an unsupported Tomcat version, we encourage you to upgrade to use version", ProbeStatus.Health.RED);
+        }
+
+        return new ProbeStatus("Could not complete Tomcat version check", ProbeStatus.Health.GREEN);
+    }
+
+    @Override
+    public void setConfig(Map<String, Object> config) {
+        // Do nothing
+    }
+
+    @Override
+    public ProbeSeverity getDefaultSeverity() {
+        return ProbeSeverity.MEDIUM;
+    }
+}
