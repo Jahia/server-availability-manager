fdaecbd8f458e628188b5e704dbd54cf408174da
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2024-07-04 09:54:49 -0400

    BACKLOG-22929: Fix startup issue when creating rendering chain test node (#146)
    * BACKLOG-22929: Fix startup issue when creating rendering chain test node
    
    * Change event listener to not be an OSGi service

--- src/main/java/org/jahia/modules/sam/healthcheck/probes/RenderingChainProbe.java
+++ src/main/java/org/jahia/modules/sam/healthcheck/probes/RenderingChainProbe.java
@@ -1,12 +1,13 @@
 package org.jahia.modules.sam.healthcheck.probes;
 
-import org.apache.jackrabbit.util.ISO8601;
 import org.jahia.api.Constants;
 import org.jahia.bin.Render;
 import org.jahia.modules.graphql.provider.dxm.node.NodeHelper;
 import org.jahia.modules.sam.Probe;
 import org.jahia.modules.sam.ProbeSeverity;
 import org.jahia.modules.sam.ProbeStatus;
+import org.jahia.modules.sam.events.SystemSiteHomeEventListener;
+import org.jahia.osgi.BundleUtils;
 import org.jahia.services.SpringContextSingleton;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRSessionFactory;
@@ -17,8 +18,8 @@ import org.jahia.services.events.JournalEventReader;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.RenderService;
 import org.jahia.services.render.Resource;
-import org.jahia.services.sites.JahiaSite;
 import org.jahia.services.sites.JahiaSitesService;
+import org.jahia.services.templates.JahiaTemplateManagerService;
 import org.jahia.settings.SettingsBean;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -31,18 +32,20 @@ import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.text.MessageFormat;
-import java.util.Calendar;
-import java.util.Locale;
 
 @Component(service = Probe.class, immediate = true)
 public class RenderingChainProbe implements Probe {
 
     private final Logger logger = LoggerFactory.getLogger(RenderingChainProbe.class);
-
     private JournalEventReader journalEventReader;
+    private JahiaTemplateManagerService templateManagerService;
+    private final SystemSiteHomeEventListener eventListener = new SystemSiteHomeEventListener();
 
-    private String textTest;
+    private final String testText = "Rendering Chain Test initialized";
+    private final String siteHomePath = "/sites/" + JahiaSitesService.SYSTEM_SITE_KEY + "/home";
+    private String nodeName;
     private String nodePath;
+
 
     /**
      * Create the rendering chain test node in LIVE workspace
@@ -50,34 +53,67 @@ public class RenderingChainProbe implements Probe {
      */
     @Activate
     public void start() throws RepositoryException {
-        textTest = "Rendering Chain Test initialized at " + ISO8601.format(Calendar.getInstance());
+        templateManagerService = BundleUtils.getOsgiService(JahiaTemplateManagerService.class, null);
+        initNodePathName();
 
-        boolean isClusterActivated = SettingsBean.getInstance().isClusterActivated();
-        String nodeSuffix = (isClusterActivated && journalEventReader != null) ? journalEventReader.getNodeId() : "";
+        // register for system site listener if rendering chain test node not yet created
         JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.LIVE_WORKSPACE, null, session -> {
-            String parentPath = "/sites/systemsite/home";
-            String nodeName = "renderingChainTest" + nodeSuffix;
-            nodePath = parentPath + "/" + nodeName;
-
-            JCRNodeWrapper testNode;
-            if (!session.nodeExists(nodePath)) {
-                logger.debug("Creating rendering chain test node at {}", nodePath);
-                JCRNodeWrapper home = session.getNode(parentPath);
-                testNode = home.addNode(nodeName, "sam:renderingChain");
-                testNode.setProperty("jcr:title", "Rendering Chain Test Node");
+            if (!session.nodeExists(getSiteHomePath())) {
+                logger.debug("Rendering Chain test node not found, registering for SystemSiteEventListener");
+                eventListener.setProbe(this);
+                templateManagerService.getTemplatePackageRegistry().handleJCREventListener(eventListener, true);
             } else {
-                testNode = session.getNode(nodePath);
+                createRenderingTestNode();
             }
-            testNode.setProperty("text", textTest);
-            session.save();
-
             return null;
         });
     }
 
+    private void initNodePathName() {
+        boolean isClusterActivated = SettingsBean.getInstance().isClusterActivated();
+        String nodeSuffix = (isClusterActivated && journalEventReader != null) ? journalEventReader.getNodeId() : "";
+        nodeName = "renderingChainTest" + nodeSuffix;
+        nodePath = siteHomePath + "/" + nodeName;
+    }
+
+    public void createRenderingTestNode() {
+        try {
+            JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.LIVE_WORKSPACE, null, session -> {
+                if (!session.nodeExists(siteHomePath)) {
+                    logger.debug("{} does not exist yet; unable to create rendering chain test node for now", siteHomePath);
+                    return null;
+                }
+
+                JCRNodeWrapper testNode;
+                if (!session.nodeExists(nodePath)) {
+                    logger.debug("Creating rendering chain test node at {}", nodePath);
+                    JCRNodeWrapper home = session.getNode(siteHomePath);
+                    testNode = home.addNode(nodeName, "sam:renderingChain");
+                    testNode.setProperty("jcr:title", "Rendering Chain Test Node");
+                    testNode.setProperty("text", testText);
+                    session.save();
+
+                    // unregister event listener after creation
+                    if (templateManagerService != null) {
+                        templateManagerService.getTemplatePackageRegistry().handleJCREventListener(eventListener, false);
+                    }
+
+                    logger.info("Rendering chain test node created at {}", nodePath);
+                }
+                return null;
+            });
+        } catch (RepositoryException e) {
+            logger.error("Error creating rendering chain test node", e);
+        }
+    }
+
+    public String getSiteHomePath() {
+        return siteHomePath;
+    }
+
     @Deactivate
     public void stop() throws RepositoryException {
-        JCRTemplate.getInstance().doExecuteWithSystemSession(session -> {
+        JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.LIVE_WORKSPACE, null, session -> {
             if (session.nodeExists(nodePath)) {
                 logger.debug("Removing rendering chain test node at {}", nodePath);
                 session.getNode(nodePath).remove();
@@ -118,10 +154,12 @@ public class RenderingChainProbe implements Probe {
             JCRSessionFactory sessionFactory = JCRSessionFactory.getInstance();
             RenderContext renderContext = new RenderContext(request, response, sessionFactory.getCurrentUser());
             JCRSessionWrapper currentUserSession = sessionFactory.getCurrentUserSession(Constants.LIVE_WORKSPACE, null);
+
             if (!currentUserSession.nodeExists(nodePath)) {
                 // something wrong happened in the setup/activate, not necessarily with the rendering chain
-                return new ProbeStatus("Rendering Chain test node not found", ProbeStatus.Health.YELLOW);
+                return new ProbeStatus("Rendering Chain test node " + nodePath + " not found", ProbeStatus.Health.YELLOW);
             }
+
             JCRNodeWrapper testNode = currentUserSession.getNode(nodePath);
             JCRNodeWrapper mainNode = NodeHelper.getNodeInLanguage(testNode, "en");
             Resource r = new Resource(mainNode, "html", "default", "module");
@@ -134,8 +172,8 @@ public class RenderingChainProbe implements Probe {
 
             response.setCharacterEncoding(SettingsBean.getInstance().getCharacterEncoding());
             renderingResult = renderService.render(r, renderContext);
-            if (!renderingResult.contains(textTest)) {
-                return new ProbeStatus(MessageFormat.format("Rendering Chain test result should have contained {0} but was {1}", textTest, renderingResult), ProbeStatus.Health.RED);
+            if (!renderingResult.contains(testText)) {
+                return new ProbeStatus(MessageFormat.format("Rendering Chain test result should have contained {0} but was {1}", testText, renderingResult), ProbeStatus.Health.RED);
             }
         } catch (Exception e) {
             return new ProbeStatus(MessageFormat.format("Rendering Chain test returns an error: {0}", e.getMessage()), ProbeStatus.Health.RED);
