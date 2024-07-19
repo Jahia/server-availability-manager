284a17689fed36fc22436c092cd53160944656bc
Jérôme Blanchard <jblanchard@jahia.com>
2024-07-19 09:13:22 +0200

    BACKLOG-23025: Fix test login sequence

--- tests/cypress/e2e/api/springUsageModule.spec.ts
+++ tests/cypress/e2e/api/springUsageModule.spec.ts
@@ -2,21 +2,23 @@ import {healthCheck} from '../../support/gql';
 
 describe('Module Spring Usage probe test', () => {
     it('Check that module spring usage probe exists and is green by default', () => {
+        cy.login();
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
             expect(r.status.health).to.eq('GREEN');
             const moduleSpringUsageProbe = r.probes.find(probe => probe.name === 'ModulesSpringUsage');
             expect(moduleSpringUsageProbe.status.health).to.eq('GREEN');
             expect(moduleSpringUsageProbe.severity).to.eq('MEDIUM');
         });
+        cy.logout();
     });
 
     // Check the probe is green, deploy a non jahia module that does not use spring and check that the probe is green, undeploy module
     it('check that installing a module without spring usage keeps the probe green', {retries: 5}, function () {
+        cy.login();
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
             expect(r.status.health).to.eq('GREEN');
         });
 
-        cy.login();
         cy.installBundle('springUsageModuleProbe/no-spring-module-8.2.0.0.jar');
         cy.runProvisioningScript([{startBundle: 'no-spring-module/8.2.0.0'}]);
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
@@ -33,11 +35,11 @@ describe('Module Spring Usage probe test', () => {
     // Deploy a module that uses spring but provided by jahia (groupId is: org.jahia.modules) and check that the probe is still green
     // Then change the probe config to include jahia provided modules and check that the probe becomes yellow
     it('check that jahia provided module even using spring does not affect probe', {retries: 5}, function () {
+        cy.login();
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
             expect(r.status.health).to.eq('GREEN');
         });
 
-        cy.login();
         cy.installBundle('springUsageModuleProbe/spring-jahia-gid-module-8.2.0.0.jar');
         cy.runProvisioningScript([{startBundle: 'spring-jahia-gid-module/8.2.0.0'}]);
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
@@ -69,11 +71,11 @@ describe('Module Spring Usage probe test', () => {
 
     // Check the probe is green, deploy a non jahia module that uses spring with beans.xml files (no import) and check that the probe is yellow, undeploy module
     it('check that module using spring beans declaration xml files are detected by probe', {retries: 5}, function () {
+        cy.login();
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
             expect(r.status.health).to.eq('GREEN');
         });
 
-        cy.login();
         cy.installBundle('springUsageModuleProbe/spring-bean-module-8.2.0.0.jar');
         cy.runProvisioningScript([{startBundle: 'spring-bean-module/8.2.0.0'}]);
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
@@ -87,11 +89,11 @@ describe('Module Spring Usage probe test', () => {
 
     // Check the probe is green, deploy a non jahia module that uses spring in imports and check that the probe is yellow, undeploy module
     it('check that module using spring osgi import package are detected by probe', {retries: 5}, function () {
+        cy.login();
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
             expect(r.status.health).to.eq('GREEN');
         });
 
-        cy.login();
         cy.installBundle('springUsageModuleProbe/spring-import-module-8.2.0.0.jar');
         cy.runProvisioningScript([{startBundle: 'spring-import-module/8.2.0.0'}]);
         healthCheck({includes: 'ModulesSpringUsage', severity: 'LOW'}).should(r => {
