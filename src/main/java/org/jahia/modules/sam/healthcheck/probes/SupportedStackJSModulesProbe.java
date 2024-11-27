c8b17818a68530243438b4c7a8f6b0a629f1a804
Baptiste Grimaud <bgrimaud@jahia.com>
2024-11-27 10:15:02 +0100

    BACKLOG-23366: Migrate to javascript-modules-engine

--- src/main/java/org/jahia/modules/sam/healthcheck/probes/SupportedStackJSModulesProbe.java
+++ src/main/java/org/jahia/modules/sam/healthcheck/probes/SupportedStackJSModulesProbe.java
@@ -20,18 +20,18 @@ public class SupportedStackJSModulesProbe implements Probe {
 
     @Override
     public ProbeStatus getStatus() {
-        Bundle npmModulesEngineBundle = BundleUtils.getBundleBySymbolicName("npm-modules-engine", null);
+        Bundle npmModulesEngineBundle = BundleUtils.getBundleBySymbolicName("javascript-modules-engine", null);
         boolean isNpmModulesEngineStarted = npmModulesEngineBundle != null && npmModulesEngineBundle.getState() == Bundle.ACTIVE;
 
         String vmVendor = System.getProperty("java.vm.vendor", "Unknown");
         Version jvmVersion = new Version(System.getProperty("java.version", "Unknown"));
         Version graalVMVersion = new Version(System.getProperty("org.graalvm.version", "Unknown"));
 
-        // This probe is only relevant for Jahia 8.2.0.0+ in which npm-modules-engine is available. 
+        // This probe is only relevant for Jahia 8.2.0.0+ in which javascript-modules-engine is available. 
         // Not testing Jahia version since it is not to be backported to older versions of SAM.
         ProbeStatus status = new ProbeStatus("No issues to report", ProbeStatus.Health.GREEN);
         if (!isNpmModulesEngineStarted) {
-            status = ProbeStatusUtils.aggregateStatus(status, "The environment is not running JS modules (npm-modules-engine stopped or not present)", ProbeStatus.Health.GREEN);
+            status = ProbeStatusUtils.aggregateStatus(status, "The environment is not running JS modules (javascript-modules-engine stopped or not present)", ProbeStatus.Health.GREEN);
         } else if (System.getProperty("org.graalvm.version") == null) {
             status = ProbeStatusUtils.aggregateStatus(status, String.format("GraalVM not detected on the environment (detected vendor: %s with JVM version: %s), after switching to GraalVM 22.3.3+ with JVM version 17+, make sure to enable the Javascript extension", vmVendor, jvmVersion), ProbeStatus.Health.RED);
         } else { 
